e6f924e082df265326c2cbb2eccfa52339da9d26
zxd2629546 <964050436@qq.com>
2015-07-04 21:16:01 +0800

    Android mine game
    mine for myself

---
+++ app/src/main/java/com/zxd/mine/util/MineMapUtil.java
@@ -0,0 +1,13 @@
+package com.zxd.mine.util;
+
+import com.zxd.mine.m.MineBlock;
+import com.zxd.mine.m.MineMap;
+
+/**
+ * Created by zxd on 2015/7/2.
+ */
+public class MineMapUtil {
+    public static void updateMineMap(MineMap map, MineBlock block){
+
+    }
+}
